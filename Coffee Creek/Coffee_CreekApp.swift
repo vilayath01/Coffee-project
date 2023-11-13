a40478ca234b1403659e90cd834ba3ee980a6d46
Vilayath Mohammed <vilayath.mohammed@arq.group>
2023-11-13 11:21:18 +1100

    Initial Commit

---
+++ Coffee Creek/Coffee_CreekApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_CreekApp.swift
+//  Coffee Creek
+//
+//  Created by vila on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_CreekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
